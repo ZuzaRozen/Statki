aeb100e142f6e99fa2fb8e7464a1d59fef05ed95
ZuzaRozen <127381282+ZuzaRozen@users.noreply.github.com>
2026-05-06 13:30:24 +0200

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,10 +1,11 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <ctime>
 #include "Gra.hpp"
 #include <iostream>
 using namespace sf;
 using namespace std;
+
 
 int main() {
     srand(time(0));
@@ -13,18 +14,57 @@ int main() {
     sf::Font font;
     if (!font.loadFromFile("bahnschrift.ttf")) return -1;
 
-    Gra gra;
-    bool kolejGracza = 1;
-    bool trybDemo = 0;
+    bool wybranyTryb = false;
+    int rozmiarPlanszy = 11;
+    int cel = 20;
+
+    Text menuText;
+    menuText.setFont(font);
+    menuText.setCharacterSize(50);
+    menuText.setString("Wybierz tryb:\n1. Standard (10x10)\n2. Demo (5x5)");
+    menuText.setPosition(500, 400);
+
+    Text instrukcje;
     float cellSize = 80.f;
     float margin = 40.f;
     float offsetPrzeciwnika = 950.f;
-    int ile_statkow = 0;
-    Text instrukcje;
     instrukcje.setFont(font);
     instrukcje.setCharacterSize(40);
-    instrukcje.setPosition(margin, 11 * cellSize + 80);
+    instrukcje.setPosition(margin, rozmiarPlanszy * cellSize + 80);
+
+    while (window.isOpen() && !wybranyTryb) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) window.close();
+            if (event.type == Event::KeyPressed) {
+                if (event.key.code == Keyboard::Num1) {
+                    rozmiarPlanszy = 11;
+                    cel = 20;
+                    wybranyTryb = true;
+                }
+                if (event.key.code == Keyboard::Num2) {
+                    rozmiarPlanszy = 6;
+                    cel = 4;    
+                    wybranyTryb = true;
+                }
+            }
+        }
+        window.clear();
+        window.draw(menuText);
+        window.display();
+    }
+
+    Gra gra(rozmiarPlanszy, cel);
+    if (rozmiarPlanszy == 6) {
+        instrukcje.setString("Tryb DEMO: Ustaw statki (kliknij " + to_string(cel) + " razy)");
+    }
+
+    bool kolejGracza = 1;
+
+    int ile_statkow = 0;
+
     instrukcje.setString("Ustaw swoje statki (kliknij 20 razy)");
+
 
 
     while (window.isOpen()) {
@@ -38,22 +78,22 @@ int main() {
                     int kol = (event.mouseButton.x - margin) / cellSize;
                     int wiersz = (event.mouseButton.y - margin) / cellSize;
 
-                    if (wiersz >= 1 && wiersz <= 10 && kol >= 1 && kol <= 10 && !gra.PlanszaUzytkownika.maSasiadowSkos(wiersz, kol)
-                        && !gra.PlanszaUzytkownika.czyJuzStrzelano(wiersz, kol) && !gra.PlanszaUzytkownika.zaDuzoRozmiaru(wiersz, kol)) {
+                    if (wiersz >= 1 && wiersz < rozmiarPlanszy && kol >= 1 && kol < rozmiarPlanszy && !gra.PlanszaUzytkownika.maSasiadowSkos(wiersz, kol, rozmiarPlanszy)
+                        && !gra.PlanszaUzytkownika.czyJuzStrzelano(wiersz, kol) && !gra.PlanszaUzytkownika.zaDuzoRozmiaru(wiersz, kol, rozmiarPlanszy)) {
                         gra.PlanszaUzytkownika.ustawStatekNaWsp(wiersz, kol);
                         ile_statkow++;
-                        instrukcje.setString("Ustaw swoje statki (kliknij 20 razy)");
+                        instrukcje.setString("Ustaw swoje statki (kliknij " + to_string(cel) + " razy)");
                     }
                     else instrukcje.setString("Niepoprawnie zaznaczone pole.");
                 }
-                if (ile_statkow == 20) {
-                    if (gra.PlanszaUzytkownika.poprawnieUstawione()) {
+                if (ile_statkow == gra.celTrafien) {
+                    if (gra.PlanszaUzytkownika.poprawnieUstawione(rozmiarPlanszy)) {
                         instrukcje.setString("Twoja kolej! Strzelaj w plansze po prawej stronie.");
                         gra.gotowa = true;
                     }
                     else {
                         instrukcje.setString("Ustaw swoje statki poprawnie");
-                        gra.PlanszaUzytkownika.wyczyscPlansze();
+                        gra.PlanszaUzytkownika.wyczyscPlansze(rozmiarPlanszy);
                         ile_statkow = 0;
                     }
                 }
@@ -67,7 +107,7 @@ int main() {
                     int kol = (event.mouseButton.x - margin - offsetPrzeciwnika) / cellSize;
                     int wiersz = (event.mouseButton.y - margin) / cellSize;
 
-                    if (wiersz >= 1 && wiersz <= 10 && kol >= 1 && kol <= 10) {
+                    if (wiersz >= 1 && wiersz < rozmiarPlanszy && kol >= 1 && kol < rozmiarPlanszy) {
                         if (!gra.kompPok.czyJuzStrzelano(wiersz, kol)) {
 
                             gra.wykonajStrzal(wiersz, kol);
@@ -88,8 +128,8 @@ int main() {
         window.clear(Color(30, 30, 30));
 
         // Rysowanie planszy użytkownika (lewa strona)
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
+        for (int i = 0; i < rozmiarPlanszy; i++) {
+            for (int j = 0; j < rozmiarPlanszy; j++) {
                 char stan = gra.PlanszaUzytkownika.getPole(i, j);
                 RectangleShape cell(Vector2f(cellSize - 2, cellSize - 2));
                 cell.setPosition(j * cellSize + margin, i * cellSize + margin);
@@ -111,9 +151,10 @@ int main() {
         }
 
         //Rysowanie planszy komputera (prawa strona)
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
+        for (int i = 0; i < rozmiarPlanszy; i++) {
+            for (int j = 0; j < rozmiarPlanszy; j++) {
                 char stan = gra.kompPok.getPole(i, j);
+
                 RectangleShape cell(Vector2f(cellSize - 2, cellSize - 2));
                 //przesuwamy o offsetPrzeciwnika!!!
                 cell.setPosition(j * cellSize + margin + offsetPrzeciwnika, i * cellSize + margin);
@@ -138,10 +179,10 @@ int main() {
         Text status;
         status.setFont(font);
         status.setCharacterSize(40);
-        status.setPosition(margin+1000, 11 * cellSize + 80);        //napis przesunięty pod planszę komputera, Przemyśleć?
+        status.setPosition(margin+1000, rozmiarPlanszy * cellSize + 80);        //napis przesunięty pod planszę komputera, Przemyśleć?
         if (gra.koniec) {
-            status.setString(gra.trafienia >= gra.celTrafien ? "WYGRANA!" : "PRZEGRANA!");
-            status.setFillColor(gra.trafienia >= gra.celTrafien ? Color::Green : Color::Red);
+            status.setString(gra.trafienia >= cel ? "WYGRANA!" : "PRZEGRANA!");
+            status.setFillColor(gra.trafienia >= cel ? Color::Green : Color::Red);
         }
         /*else {
             status.setString("Pudla: " + to_string(gra.pudla) + " / " + to_string(gra.maksPudel));
